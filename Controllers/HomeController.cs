920a6dc293abfb978c6b58ec3e14db7080b47725
ontedi <universityofcu@gmail.com>
2022-01-16 01:30:33 +0300

    master

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,46 +7,46 @@ namespace RabbitMQConsumer.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+public IActionResult Index()
+{
+    string strKisiListesi = string.Empty;
+    var cfBaglantiBilgileri = new ConnectionFactory()
+    {
+        HostName = "localhost",
+        Port = 5672,
+        UserName = "guest",
+        Password = "guest"
+    };
+
+    using (IConnection cfBaglanti = cfBaglantiBilgileri.CreateConnection())
+    using (IModel chnKanal = cfBaglanti.CreateModel())
+    {
+        chnKanal.QueueDeclare
+        (
+            queue: "bilgilendirme-mesajlari-2022-01-15",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        var ebcKuyruklar = new EventingBasicConsumer(chnKanal);
+
+        ebcKuyruklar.Received += (model, mq) =>
         {
-            string strKisiListesi = string.Empty;
-            var cfBaglantiBilgileri = new ConnectionFactory()
-            {
-                HostName = "localhost",
-                Port = 5672,
-                UserName = "guest",
-                Password = "guest"
-            };
+            var MesajGovdesi = mq.Body;
+            strKisiListesi = Encoding.UTF8.GetString(MesajGovdesi.ToArray());
+        };
 
-            using (IConnection cfBaglanti = cfBaglantiBilgileri.CreateConnection())
-            using (IModel chnKanal = cfBaglanti.CreateModel())
-            {
-                chnKanal.QueueDeclare
-                (
-                    queue: "bilgilendirme-mesajlari-2022-01-15",
-                    durable: false,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null
-                );
-
-                var ebcKuyruklar = new EventingBasicConsumer(chnKanal);
-
-                ebcKuyruklar.Received += (model, mq) =>
-                {
-                    var MesajGovdesi = mq.Body;
-                    strKisiListesi = Encoding.UTF8.GetString(MesajGovdesi.ToArray());
-                };
-
-                chnKanal.BasicConsume
-                (
-                    queue: "bilgilendirme-mesajlari-2022-01-15",
-                    autoAck: false, // true ise mesaj otomatik olarak kuyruktan silinir
-                    consumer: ebcKuyruklar
-                );
-            }
-            TempData["KisiListesi"] = strKisiListesi;
-            return View();
-        }
+        chnKanal.BasicConsume
+        (
+            queue: "bilgilendirme-mesajlari-2022-01-15",
+            autoAck: false, // true ise mesaj otomatik olarak kuyruktan silinir
+            consumer: ebcKuyruklar
+        );
+    }
+    TempData["KisiListesi"] = strKisiListesi;
+    return View();
+}
     }
 }
